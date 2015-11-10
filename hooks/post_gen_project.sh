c263c02573eafd453a2737e5592ed444d5a293bf
Adam Shimali <adam.shimali@gmail.com>
2015-11-10 11:24:58 +0000

    put post hooks for sass and cleanup back in

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,10 +7,10 @@ make -f scripts/govuk-assets-flask/Makefile govuk_assets_all
 mv application/static/govuk_template/assets/* application/static/
 rm -rf application/static/govuk_template
 
-# sass --style expanded --line-numbers --load-path  application/static/govuk_frontend_toolkit/stylesheets  application/static/govuk_elements/public/sass/main.scss application/static/stylesheets/govuk_elements.css
+sass --style expanded --line-numbers --load-path  application/static/govuk_frontend_toolkit/stylesheets  application/static/govuk_elements/public/sass/main.scss application/static/stylesheets/govuk_elements.css
 
 # this might be a bit to brutal but don't really need this hanging around
 # can maybe put this into a makefile for the project rather than a post get
 # task
-# rm -rf application/static/govuk_frontend_toolkit
-# rm -rf application/static/govuk_elements
+rm -rf application/static/govuk_frontend_toolkit
+rm -rf application/static/govuk_elements
