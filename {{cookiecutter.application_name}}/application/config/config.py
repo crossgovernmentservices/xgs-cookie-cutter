a0bbc164a58707638bcf2d32341581ac72e01fed
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-12-01 16:30:29 +0000

    Correct APP_ROOT
    the calculation of APP_ROOT was incorrect, needed to be one level higher
    up in the folder heirarchy.

--- {{cookiecutter.application_name}}/application/config/config.py
+++ {{cookiecutter.application_name}}/application/config/config.py
@@ -2,7 +2,7 @@
 import os
 
 class Config(object):
-    APP_ROOT = os.path.abspath(os.path.dirname(__file__))
+    APP_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_ROOT, os.pardir))
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
